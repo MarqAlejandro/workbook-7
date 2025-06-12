d65e13818130942948e4a425a403468147d3338c
MarqAlejandro <alejandromarq3@gmail.com>
2025-06-11 21:41:31 -0700

    Update workshop_car_dealership.sql

--- workshop/workshop_car_dealership.sql
+++ workshop/workshop_car_dealership.sql
@@ -29,7 +29,21 @@ CREATE TABLE MARQ.sales_contracts(
     date NVARCHAR(10),
     name NVARCHAR(18),
     email NVARCHAR(30),
-    vin INT
+    vin INT,
+    total_price DECIMAL(10,2),
+    leased BIT DEFAULT 0,
+    monthly DECIMAL(10,2)
+)
+CREATE TABLE MARQ.lease_contracts(
+    contract_id INT PRIMARY KEY IDENTITY(1,1),
+    date NVARCHAR(10),
+    name NVARCHAR(18),
+    email NVARCHAR(30),
+    vin INT,
+    expected_ending_value DECIMAL(10,2),
+    lease_fee DECIMAL(10,2),
+    total_price DECIMAL(10,2),
+    monthly_fee DECIMAL(10,2)
 )
 INSERT INTO MARQ.dealerships( name, address, phone)
 VALUES('DEALERS','123 TEST AVE', '123-456-7890')
